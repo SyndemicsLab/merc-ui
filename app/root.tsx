d8506ff427484db0f08061a62143e33684b10c6d
Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
2024-05-28 12:33:56 -0400

    Add branding, add SPA-mode

--- app/root.tsx
+++ app/root.tsx
@@ -68,9 +68,12 @@ export default function App() {
 			</li>
 		    </ul>
 		</div>
-
 		<div id="content">
 		    <Outlet />
+		    <hr/>
+		    <div id="branding">
+			This application is part of a collaboration between Weill Cornell Medicine, Boston Medical Center, and Brown University School of Public Health. RESPOND is funded by the National Institute on Drug Abuse (NIDA) R01DA046527. This web application is also funded by NIDA as a HEAL Data2Action Modeling and Economic Resource Center (HEAL D2A MERC) U24DA057650 project and by the Center for Health Economics of Treatment Interventions for Substance Use Disorder, HCV, and HIV (CHERISH) P30DA040500.
+		    </div>
 		</div>
 		<ScrollRestoration />
 		<Scripts />
