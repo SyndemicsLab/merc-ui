1c7fbae9d5a3e3d6229915af6d096bdcc8acce9e
Milo Park <minpark@crc-dot1x-nat-10-239-152-176.bu.edu>
2024-06-20 23:45:13 -0400

    pr changes fixed

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,7 @@
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, Link, useLocation } from "@remix-run/react";
 import appStylesHref from "./app.css?url";
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import respondlogo from "./images/respondlogo.png"
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
@@ -11,8 +12,36 @@ export const meta: MetaFunction = () => {
   return [
     { rel: "icon", href: "/favicon.ico" },
     { title: "RESPOND Simulation" },
-    { name: "description", content: "The Syndemics Lab at Boston Medical Center's RESPOND simulation as a web application." },
+    { name: "description", content: "The Syndemics Lab at Boston Medical Center's RESPOND simulation as a web application." }
   ];
+};
+
+const NavigationMenu = () => {
+  const menuItems = [
+    { text: "Home", link: "/" },
+    { text: "Simulation Model", link: "/simulationmodel" },
+    { text: "About Us", link: "/about" },
+    { text: "Model Materials", link: "/modelmaterials" },
+    { text: "Publications", link: "/publications" },
+    { text: "Contact Us", link: "/contactus" }
+  ];
+
+  const location = useLocation();
+
+  return (
+    <header className="header">
+      <Link to="/">
+        <img className="logo" src={respondlogo} alt="RESPOND Simulation" />
+      </Link>
+      <nav className="nav">
+        {menuItems.map((item, index) => (
+          <Link key={index} to={item.link} className={`nav-button ${location.pathname === item.link ? 'active' : ''}`}>
+            {item.text}
+          </Link>
+        ))}
+      </nav>
+    </header>
+  );
 };
 
 export default function App() {
@@ -25,6 +54,7 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <NavigationMenu />
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,47 +1,6 @@
-import { Link, useLocation } from "@remix-run/react";
-import respondLogo from "../images/respondlogo.png";
-
-interface MenuItemProps {
-  text: string;
-  link: string;
-}
-
-const NavigationMenu: React.FC = () => {
-  const menuItems: MenuItemProps[] = [
-    { text: 'Home', link: '/' },
-    { text: 'Simulation Model', link: '/simulation' },
-    { text: 'About us', link: '/about' },
-    { text: 'Model Materials', link: '/modelmaterials' },
-    { text: 'Publications', link: '/publications' },
-    { text: 'Contact us', link: '/contact' },
-  ];
-
-  const location = useLocation();
-
-  return (
-    <header className="header">
-      <img className="logo" loading="lazy" src={respondLogo} alt="RESPOND Simulation" />
-      <nav className="nav">
-        {menuItems.map((item, index) => (
-          <NavItem key={index} text={item.text} link={item.link} isActive={location.pathname === item.link} />
-        ))}
-      </nav>
-    </header>
-  );
-};
-
-const NavItem: React.FC<{ text: string; link: string; isActive: boolean }> = ({ text, link, isActive }) => {
-  return (
-    <Link to={link} className={`nav-button ${isActive ? 'active' : ''}`} tabIndex={0}>
-      {text}
-    </Link>
-  );
-};
-
 export default function Index() {
   return (
     <div>
-      <NavigationMenu />
       <div id="welcome">
       </div>
     </div>
