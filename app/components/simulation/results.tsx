c49839aeddacb36880a5853d6ad233b9c872b237
Dimitri Baptiste <ddbaptiste@users.noreply.github.com>
2025-02-23 18:39:41 -0500

    Small tweaks to results

--- app/components/simulation/results.tsx
+++ app/components/simulation/results.tsx
@@ -14,25 +14,24 @@ export default function Results() {
     return (
         <Dialog>
             <DialogTrigger asChild>
-                <Button variant="outline" className="run-text">▶ RUN</Button>
+                <Button variant="outline" className="run-text">RUN</Button>
             </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
+            <DialogContent className="bg-white sm:max-w-[425px]">
                 <DialogHeader>
-                    <DialogTitle>Edit profile</DialogTitle>
+                    <DialogTitle>Simulation Results</DialogTitle>
                     <DialogDescription>
                         Make changes to your profile here. Click save when you're done.
                     </DialogDescription>
                 </DialogHeader>
                 <div className="grid gap-4 py-4">
                     <div className="grid grid-cols-4 items-center gap-4">
-
                     </div>
                     <div className="grid grid-cols-4 items-center gap-4">
 
                     </div>
                 </div>
                 <DialogFooter>
-                    <Button type="submit">Save changes</Button>
+                    <Button type="submit">Download Results</Button>
                 </DialogFooter>
             </DialogContent>
         </Dialog>
