3500f9c0b56a2043df60712fdc0a771b4d9d314d
Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
2025-05-28 17:08:49 -0400

    Fixed a new scrollbar css issue and more api/ui progress.

--- app/components/home/questionnaire.tsx
+++ app/components/home/questionnaire.tsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
 import { Form } from "react-router";
 
+import type {
+    QuestionnaireForm
+} from "@components/home/types/questionnaire_form";
+import {
+    createQuestionnaireForm
+} from "@components/home/types/questionnaire_form";
+import StateDropdown from "@components/ui/statedropdown";
 import {
     Dialog,
     DialogContent,
@@ -7,41 +15,50 @@ import {
     DialogTitle,
     DialogDescription
 } from "@components/ui/dialog";
-
-import StateDropdown from "@components/ui/statedropdown";
 import { Input } from "@components/ui/input";
-import { useState } from "react";
-import type { QuestionnaireForm } from "@components/home/types/questionnaire_form";
-import { createQuestionnaireForm } from "@components/home/types/questionnaire_form";
+
+function DropdownQuestion(
+    { name, question, children }:
+    { name: string, question: string, children: Function }
+) {
+    return(
+	<>
+	    <span className="dialog-question">
+		{question}
+	    </span>
+	    {children}
+	</>
+    );
+}
 
 function MultiResponseQuestion(
     { name, question, responses, lastOther = false }:
     { name: string, question: string, responses: string[], lastOther?: boolean }) {
+    const responseOptions = responses.map((response, index) => {
+	let target: string = name + '_' +
+	    response
+	    .replace(/(\s|-)/, "")
+	    .replace(/(^\w+).*/, "$1")
+	    .replace(/\w/g, x => x.toLowerCase());
+	return(
+	    <li key={index}>
+		<input type="checkbox" className="dialog-checkbox" name={name} />
+		<span className="dialog-response">{`${response} (${target})`}
+		    { index === (responses.length - 1) && lastOther ? (
+			<input className="dialog-input rounded-md"
+			       type="text"
+			       name={`${name}OtherText`}
+			       placeholder={name.charAt(0).toUpperCase() + name.slice(1)}
+			/>
+		    ): null}
+		</span>
+	    </li>
+	);
+    });
     return (
 	<>
-            <span className="dialog-question">
-		{question}
-            </span>
-            <ul>
-		{responses.map((response, index) => {
-		    return(
-			<>
-			    <li>
-				<input type="checkbox" className="dialog-checkbox" name={name} />
-			        <span className="dialog-response">{response}
-			            { index === (responses.length - 1) && lastOther ? (
-					<input className="dialog-input rounded-md"
-					       type="text"
-				               name={`${name}OtherText`}
-					       placeholder={name.charAt(0).toUpperCase() + name.slice(1)}
-					/>
-			            ): null}
-				</span>
-			    </li>
-			</>
-		    );
-		})}
-            </ul>
+            <span className="dialog-question">{question}</span>
+            <ul>{responseOptions}</ul>
 	</>
     );
 }
@@ -85,11 +102,12 @@ const Questionnaire = () => {
 			    ]}
 			    lastOther={true}
 			/>
-                        <span className="dialog-question">
-                            What US State are you most interested in researching?
-                        </span>
-                        <StateDropdown />
-                        <Input type="hidden" name="usState" id="usState" required />
+			<DropdownQuestion
+			    name="usState"
+			    question="What US State are you most interested in researching?"
+			>
+			    <StateDropdown />
+			</DropdownQuestion>
 			<MultiResponseQuestion
 			    name="occupation"
 			    question="What is your occupation or field of work? (Select all that apply)"
@@ -111,7 +129,7 @@ const Questionnaire = () => {
                         type="hidden"
                         name="questionnaireVisibility"
                         value="hidden" />
-                    <button type="submit">Submit</button>
+                    <button type="submit" onClick={() => {alert(JSON.stringify(formContents))}}>Submit</button>
                 </Form>
             </DialogContent>
         </Dialog>
