a3b4ddbbee801d4b6abf3caf63ae9c4def8c5103
Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
2025-06-09 14:06:00 -0400

    Fix cookie creation when questionnaire is submitted.

--- app/components/home/questionnaire.tsx
+++ app/components/home/questionnaire.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, useNavigate } from "react-router";
+import { Form, useFetcher } from "react-router";
 
 import Dropdown from "@components/ui/dropdown";
 import States from "@components/ui/questionnaire";
@@ -90,38 +90,7 @@ function MultiResponseQuestion(
 
 const Questionnaire = () => {
     const [open, setOpen] = useState(true);
-    const navigate = useNavigate();
-
-    async function handleSubmit(e) {
-	e.preventDefault();
-
-	const formData = new FormData(e.target);
-
-	const formJson = Object.fromEntries(formData.entries());
-	for (var x in formJson) {
-	    if (formJson[x] === "on") {
-		formJson[x] = true;
-	    }
-	    if (formJson[x] === "") {
-		formJson[x] = null;
-	    }
-	}
-
-	const request = fetch(
-	    "http://127.0.0.1:8000/SubmitQuestionnaire",
-	    {
-		method: e.target.method,
-		mode: "cors",
-		body: JSON.stringify(formJson),
-		headers: {
-		    "Content-Type": "application/json",
-		}
-	    }
-	);
-
-	navigate('/');
-	setOpen(false);
-    }
+    let fetcher = useFetcher();
 
     return (
         <Dialog open={open} onOpenChange={setOpen}>
@@ -130,7 +99,7 @@ const Questionnaire = () => {
 		    <DialogTitle>Questionnaire</DialogTitle>
 		    <DialogDescription>Please help us serve you better by answering a few quick questions.</DialogDescription>
                 </DialogHeader>
-                <Form method="post" onSubmit={handleSubmit}>
+                <fetcher.Form method="post">
 		    <div className="questionnaire">
 			<MultiResponseQuestion
 			    name="purpose"
@@ -166,8 +135,13 @@ const Questionnaire = () => {
 			    lastOther={true}
 			/>
 		    </div>
+		    <input
+			name="questionnaireVisibility"
+			type="hidden"
+			value="hidden"
+		    />
 		    <button type="submit">Submit</button>
-                </Form>
+                </fetcher.Form>
 	    </DialogContent>
         </Dialog>
     );
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -14,7 +14,7 @@ import { userPrefs } from "~/cookies";
 export async function loader({ request }: Route.LoaderArgs) {
     const cookieHeader = request.headers.get("Cookie");
     const cookie = (await userPrefs.parse(cookieHeader)) || {};
-    console.log(cookie);
+
     if (cookie.showQuestionnaire === undefined) {
         cookie.showQuestionnaire = true;
     }
@@ -25,6 +25,30 @@ export async function action({ request }: Route.ActionArgs) {
     const cookieHeader = request.headers.get("Cookie");
     const cookie = (await userPrefs.parse(cookieHeader)) || {};
     const bodyParams = await request.formData();
+
+    const formJson = Object.fromEntries(bodyParams.entries());
+    for (var x in formJson) {
+	if (formJson[x] === "on") {
+	    formJson[x] = true;
+	}
+	if (formJson[x] === "") {
+	    formJson[x] = null;
+	}
+    }
+
+    delete formJson.questionnaireVisibility;
+
+    const response = await fetch(
+	"http://127.0.0.1:8000/SubmitQuestionnaire",
+	{
+	    method: "POST",
+	    mode: "cors",
+	    body: JSON.stringify(formJson),
+	    headers: {
+		"Content-Type": "application/json",
+	    }
+	}
+    );
 
     if (bodyParams.get("questionnaireVisibility") === "hidden") {
         cookie.showQuestionnaire = false;
