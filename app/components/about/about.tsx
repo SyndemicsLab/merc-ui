a68c49d4f797d1767ba6b37c2aed052644276904
Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
2025-10-30 11:29:08 -0400

    [Enhancements] "About RESPOND" Information Cards (#46)
    * Major changes to images in "About RESPOND", minor updates across css
    
    * Switch from `DetailSVG` to more appropriately named `InfoCard`
    
    * Correct indentation in `about`.
    
    * Deleting images that are no longer used
    
    * Remove imports related to images, add a blur effect to `InfoCard`s
    
    * Address PR comment, fix change in nav menu indicator colors

--- app/components/about/about.tsx
+++ app/components/about/about.tsx
@@ -1,8 +1,36 @@
 import { Link } from "react-router";
-import about1 from '~/images/about1.svg';
-import about2 from '~/images/about2.svg';
-import about3 from '~/images/about3.svg';
-import about4 from '~/images/about4.svg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faPeopleLine,
+    faHexagonNodes,
+    faBookMedical,
+    faHouseMedical
+} from "@fortawesome/free-solid-svg-icons";
+
+interface InfoCardProps {
+    icon: ReactNode;
+    caption: string;
+    overlay: string;
+    className?: string;
+}
+
+function InfoCard({ icon, caption, overlay, className = "" }: InfoCardProps) {
+    // handle class names - add leading space only if className is nonempty
+    let classes = className === "" ? className : ` ${className}`;
+    return(
+        <div className={`info-card${classes}`}>
+            <div className="info-card-content">
+                <div className="icon">
+                    {icon}
+                </div>
+                <span>{caption}</span>
+            </div>
+            <div className="info-card-overlay">
+                {overlay}
+            </div>
+        </div>
+    );
+}
 
 const About = () => {
     return (
@@ -14,10 +42,28 @@ const About = () => {
                         RESPOND (Researching Effective Strategies to Prevent Opioid Death) is a cohort-based state transition simulation model designed to help understand and address opioid use. It tracks a population at high risk for opioid use, modeling how people start and stop medication for opioid use disorder (MOUD) and how this affects outcomes like overdose, mortality, quality of life, and costs. RESPOND can evaluate the impact and cost-effectiveness of different strategies, including expanding MOUD access. While it uses data specific to Massachusetts, it can be adapted for other locations with the right data. Explore our <Link to="https://syndemicslab.github.io/respond">documentation</Link> and <Link to="#modelmaterials">model materials</Link> to learn more.
                     </p>
                     <div className="about-svg-grid">
-                        <img className="about-svg" src={about1} alt="RESPOND Feature 1" />
-                        <img className="about-svg" src={about2} alt="RESPOND Feature 2" />
-                        <img className="about-svg" src={about3} alt="RESPOND Feature 3" />
-                        <img className="about-svg" src={about4} alt="RESPOND Feature 4" />
+                        <InfoCard
+                            caption="Models populations at high risk for opioid use disorder"
+                            overlay="More information about RESPOND"
+                            icon={<FontAwesomeIcon icon={faPeopleLine} />}
+                        />
+                        <InfoCard
+                            caption="Simulates groups of people moving between health states"
+                            overlay="More information about RESPOND"
+                            icon={<FontAwesomeIcon icon={faHexagonNodes} />}
+                            className="blue-card"
+                        />
+                        <InfoCard
+                            caption="Analyzes the impact of policy changes on health outcomes and costs"
+                            overlay="More information about RESPOND"
+                            icon={<FontAwesomeIcon icon={faBookMedical} />}
+                        />
+                        <InfoCard
+                            caption="Presents information about the impact of medication on substance use"
+                            overlay="More information about RESPOND"
+                            icon={<FontAwesomeIcon icon={faHouseMedical} />}
+                            className="blue-card"
+                        />
                     </div>
                 </section>
             </main>
