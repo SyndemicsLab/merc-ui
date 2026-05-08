4694fb5e9cbabf3af5c12db381d67aacb5d03045
Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
2026-05-08 13:42:25 -0400

    [Enhancement] Shorten maximum simulation duration (#63)
    * Change max duration for alpha
    
    * Revise to 7 years due to inconsistent behavior with 8

--- app/routes/simulation.tsx
+++ app/routes/simulation.tsx
@@ -616,7 +616,8 @@ function Input({
             inputVar: "duration",
             inputText: "Simulation Duration (Weeks)",
             min: 1,
-            max: 2600,
+            // limiting duration to 7 years in Alpha
+            max: 364,
             step: 1,
             defaultValue: inputs.duration,
             action: (value: number) =>
