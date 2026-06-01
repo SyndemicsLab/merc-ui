49e990de1b20e9470be538c13a5640a793f26927
Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
2026-06-01 16:09:26 -0400

    remove eslint-plugin-react due to dependency conflicts

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,12 +1,10 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
     tseslint.configs.recommended,
-    pluginReact.configs.flat["jsx-runtime"],
     // {
     //     ...pluginReact.configs.flat.recommended,
     //     settings: {
@@ -19,7 +17,6 @@ export default defineConfig([
         files: ["**/*.{js, mjs, cjs, ts, mts, cts, jsx, tsx}"],
         plugins: {
             js,
-            react: pluginReact,
         },
         extends: ["js/recommended"],
         languageOptions: { globals: globals.browser },
