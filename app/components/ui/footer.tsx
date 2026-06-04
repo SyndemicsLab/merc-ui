3bda4073983e7c5bb90aaa0cf35af34fcfe117dc
Ryon Das <ryon.d.das@gmail.com>
2026-06-04 11:00:57 -0400

    added privacy policy in footer

--- app/components/ui/footer.tsx
+++ app/components/ui/footer.tsx
@@ -59,6 +59,10 @@ const Footer = () => {
                     </div>
                 </div>
                 <p className="footer-description">
+                    This tool only stores your data for the duration of the 
+                    model run and then immediately deletes it. No data 
+                    (input or output) is persisted in our records.
+                    
                     This application is part of a collaboration between Weill
                     Cornell Medicine, Boston Medical Center, and Brown
                     University School of Public Health. RESPOND is funded by the
