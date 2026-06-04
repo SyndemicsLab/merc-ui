97289d741bec91d953ab23b1019191fcb0e06d91
Ryon Das <ryon.d.das@gmail.com>
2026-06-04 10:23:10 -0400

    Removed Hi Ryon Test and added Disclaimer

--- app/routes/simulation.tsx
+++ app/routes/simulation.tsx
@@ -743,8 +743,11 @@ function Input({
                     <DialogHeader>
                         <DialogTitle>Simulation Results</DialogTitle>
                         <DialogDescription>
-                            Hi Ryon! It may take several minutes for the model to execute
-                            and for results to populate.
+                            It may take several minutes for the model to execute
+                            and for results to populate. This tool is a simulation model. 
+                            The accuracy of the numbers are reflective of the provided 
+                            data and the assumptions made in the model structure. 
+                            These results should not be used as a guaranteed outcome.
                         </DialogDescription>
                     </DialogHeader>
                     <div className="results-main flex flex-col">
