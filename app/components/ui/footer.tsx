e4c4463b94d3a83c1fbaa697bb66e1f63dd87e3c
Ryon Das <ryon.d.das@gmail.com>
2026-06-05 09:33:58 -0400

    changed paragraph formatting/added margin bottom

--- app/components/ui/footer.tsx
+++ app/components/ui/footer.tsx
@@ -66,7 +66,7 @@ const Footer = () => {
                         (input or output) is persisted in our records.
                     </p>
 
-                    <p>&nbsp;</p>
+                    <br/>
 
                     <p className="footer-description">
                         This application is part of a collaboration between
