9bc19ece5267784548510dea019ee5e50e4c252a
Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
2026-06-24 09:34:41 -0400

    More `home` route page adjustments

--- app/components/home/abouttool.tsx
+++ app/components/home/abouttool.tsx
@@ -6,23 +6,33 @@ export default function AboutTool() {
             <div className="home-tile">
                 <h2>About This Tool</h2>
                 <p>
-                    This website provides a simplified version of the RESPOND
-                    simulation model and aims to be a user-friendly interface
-                    for modeling the health and health economic effects of the
-                    opioid use epidemic. The goal of this tool is to help anyone
+                    This website is a user-friendly, simplified version of the
+                    RESPOND model. The goal of this tool is to help anyone
                     interested, such as academic researchers, policy-makers, and
                     more, understand the impact of and make decisions about
                     research and implementation of interventions related to
                     opioid use. Use the <Link to="/simulation">Simulation</Link>{" "}
-                    page to run our default values, or update the values to test
-                    your hypotheses.
+                    page to run our default data, which represent the state of
+                    Massachusetts, or update the values to test your own
+                    hypotheses.
                 </p>
                 <p>
                     At default, our model represents individuals who use opioids
-                    in Massachusetts. Our baseline treatment states include: No
-                    Treatment, Methadone, Buprenorphine, Naltrexone, Detox, and
-                    Detention. With data, users of the tool can add new
-                    treatment states and ask even more questions.
+                    in Massachusetts. Our baseline settings include:
+                </p>
+                <div className="list-container">
+                    <ul>
+                        <li>No Treatment</li>
+                        <li>Methadone</li>
+                        <li>Buprenorphine</li>
+                        <li>Naltrexone</li>
+                        <li>Detox</li>
+                        <li>Detention</li>
+                    </ul>
+                </div>
+                <p>
+                    With data, users of the tool can add new treatment states
+                    and ask even more questions.
                 </p>
             </div>
         </div>
