8ccc0f804fe883c5017f19595e373d19e69c4dec
Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
2026-06-24 09:17:18 -0400

    actually needed to remove future flags completely

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,13 +2,6 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
     ssr: true,
-    config: {
-        middleware: true,
-        splitRouteModules: true,
-        viteEnvironmentApi: true,
-        passThroughRequests: true,
-        trailingSlashAwareDataRequests: true,
-    },
     allowedActionOrigins: ["respond.syndemicslab.org"],
     async prerender() {
         // temporarily removing `/contact` for alpha
